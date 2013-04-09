9f8f2ac7f86220044efb2d05ce7740cf08298a79
unknown <ccik@inbox.ru>
2013-04-09 04:16:39 +0400

    fixed war creation in build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 echo Building lab1...
 javac -classpath .\lib\javax.servlet.jar -sourcepath .\lab1\war\WEB-INF\classes\ .\lab1\war\WEB-INF\classes\CheckingServlet.java
-jar cvf lab1.war .\lab1\war\
+jar cvf lab1.war -C .\lab1\war\ .\
